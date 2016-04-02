e6883a03475cbdf9af86fcc6db6f02933411aa48
Brandon <bpl5@students.uwf.edu>
2016-04-02 12:43:50 -0500

    Testing Tablet View

---
+++ app/src/main/java/com/example/brandon/kittenfactory/Models/Cat.java
@@ -0,0 +1,45 @@
+package com.example.brandon.kittenfactory.Models;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Brandon on 4/2/2016.
+ */
+
+/*
+    Enums give each item a number based on order starting from 0
+    0 is the lowest rank (recessive)
+
+ */
+public class Cat {
+
+    public enum FurColor {
+        BLACK, ORANGE, WHITE
+    }
+
+    public enum FurLength {
+        LONG, SHORT
+    }
+
+    public enum EyeColor {
+        MIX, BLUE, GREEN, ORANGE
+    }
+
+    private FurColor furColor;
+    private FurLength furLength;
+    private EyeColor eyeColor;
+
+    public Cat(FurColor furColor, FurLength furLength, EyeColor eyeColor) {
+        this.furColor = furColor;
+        this.furLength = furLength;
+        this.eyeColor = eyeColor;
+    }
+
+    public FurColor getFurColor() {
+        return furColor;
+    }
+
+    public void setFurColor(FurColor furColor) {
+        this.furColor = furColor;
+    }
+}
---
+++ app/src/main/java/com/example/brandon/kittenfactory/PickFragment.java
@@ -0,0 +1,20 @@
+package com.example.brandon.kittenfactory;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Brandon on 4/2/2016.
+ */
+public class PickFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.activity_game, container, false);
+        return v;
+    }
+}
